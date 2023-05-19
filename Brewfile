31989631397c21dc68c41bebf507294fd0eeb365
Sarah Connor <14093193+terminatingcode@users.noreply.github.com>
2023-05-19 08:17:31 +0100

    Switch to zsh and pair down dependencies

--- Brewfile
+++ Brewfile
@@ -1,49 +1,23 @@
 tap 'homebrew/bundle'
 tap 'homebrew/core'
 
-brew 'ag'
 brew 'autojump'
-brew 'bash'
 brew 'bat'
 brew 'coreutils'
 brew 'diff-so-fancy'
 brew 'direnv'
-brew 'fd'
-brew 'gdb'
-brew 'gh'
-brew 'git-crypt'
 brew 'go'
-brew 'gpg'
 brew 'gti'
-brew 'hg'
 brew 'jq'
-brew 'lastpass-cli'
-brew 'npm'
 brew 'nvim'
-brew 'ripgrep'
 brew 'shellcheck'
-brew 'sshuttle'
-brew 'terraform'
 brew 'wget'
 brew 'yq'
 brew 'watch'
 brew 'tmux'
-brew 'postgresql'
 
-cask '1password-cli'
-cask 'docker'
 cask 'flycut'
-cask 'gpg-suite'
 cask 'iterm2'
-cask 'postman'
-cask 'virtualbox'
-cask 'vagrant'
-
-tap 'heroku/brew'
-brew 'heroku'
-
-tap 'git-duet/tap'
-brew 'git-duet'
 
 tap 'neovim/neovim'
 brew 'neovim'
@@ -52,8 +26,4 @@ brew 'cmake'
 
 tap 'universal-ctags/universal-ctags'
 brew 'universal-ctags', args: ['HEAD']
-
-tap 'heroku/brew'
-brew 'heroku'
-
 
--- setup.sh
+++ setup.sh
@@ -2,31 +2,6 @@
 
 set -eo pipefail
 
-function update_bash_it {
-  echo "update bash-it"
-  git submodule update --init
-
-  "$HOME/.bash_it/install.sh" --silent --no-modify-config
-
-  set +e
-  export BASH_IT="$HOME/.bash_it"
-  # shellcheck disable=1090
-  source "$HOME/.bash_it/bash_it.sh"
-  set -e
-}
-
-function configure_bash_it {
-  echo "configure bash-it"
-  set +e
-  for completion in bash-it brew docker git ssh terraform; do
-    bash-it enable completion $completion
-  done
-
-  # shellcheck disable=SC1090
-  source "$HOME/.bash_profile"
-  set -e
-}
-
 function ensure_brew_exists {
   echo "ensuring brew exists"
   if [[ ! -x /usr/local/bin/brew ]] ; then
@@ -48,25 +23,6 @@ function update_brew_bundle {
   set -e
 }
 
-function ensureLatestBashShell {
-  if [[ ! $SHELL = "/usr/local/bin/bash" ]] ; then
-    echo "changing shell to bash"
-    if ! grep -q /usr/local/bin/bash /private/etc/shells ; then
-      echo "adding bash ${bash --version} to /etc/shells"
-      echo $(brew --prefix)/bin/bash | sudo tee -a /private/etc/shells
-    fi
-    chsh -s /usr/local/bin/bash
-  fi
-}
-
-function install_golang {
-  echo "installing Go"
-  local version="1.19.3"
-  gimme $version
-  # shellcheck disable=SC1090
-  source "$HOME/.gimme/envs/go$version.env"
-}
-
 function install_luanvim {
   echo "Ensuring that we have neovim python plugins..."
   pip3 install neovim
@@ -77,35 +33,10 @@ function install_luanvim {
   fi
 }
 
-function install_cred_alert_cli {
-  if ! curl -f "https://s3.amazonaws.com/cred-alert/cli/current-release/cred-alert-cli_darwin" > "$HOME/bin/cred-alert-cli"; then
-    echo -n "Failed to get the most recent version of the cred-alert-cli, continuing..."
-  fi
-
-  chmod +x "$HOME/bin/cred-alert-cli"
-  cred-alert-cli update
-}
-
-function install_ginkgo_gomega {
-   go install github.com/onsi/ginkgo/v2/ginkgo@latest
-}
-
-function install_code_extensions {
-  code --install-extension esbenp.prettier-vscode
-  code --install-extension dbaeumer.vscode-eslint
-  code --install-extension jebbs.platnuml
-}
-
 function main {
-  update_bash_it
-  configure_bash_it
   ensure_brew_exists
   update_brew_bundle
-  ensureLatestBashShell
-  install_golang
   install_luanvim
-  install_cred_alert_cli
-  install_ginkgo_gomega
 
   echo "All done 🥳"
 }
